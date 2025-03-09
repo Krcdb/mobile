79558a584a207955278e3225bad772b01bee9431
krcdb <memartin@student.42.fr>
2025-03-09 16:34:12 +0100

    feat: ex01

---
+++ mobileModule00/ex01/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+      ),
+      home: const MyHomePage(title: 'Ex 01'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String textToDisplay = "A simple text";
+
+  void _changeText() {
+    setState(() {
+      if (textToDisplay == "A simple text") {
+        textToDisplay = "Hello World";
+      } else {
+        textToDisplay = "A simple text";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(textToDisplay),
+            ElevatedButton(
+              onPressed: _changeText,
+              child: const Text("Click me"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
